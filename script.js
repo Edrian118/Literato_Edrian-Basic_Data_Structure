254c413cf065e7eabda34f6a526b7e24a72e43f5
Edrian118 <72645946+Edrian118@users.noreply.github.com>
2024-10-13 21:50:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let totalUsersElement = document.getElementById("totalUsers");
+let totalSaleselement = document.getElementById("totalSales");
+
+let Button = document.getElementById('Button');
+
+Button.addEventListener('click', function() {
+    
+        let totalSales = totalSaleselement.textContent;
+        let totalUsers = totalUsersElement.textContent;
+});
